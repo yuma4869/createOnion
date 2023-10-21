2680900db75c5de601689ef783974c014b1c2aa8
yuma4869 <tttyyy76@outlook.jp>
2023-10-21 20:04:28 +0900

    seo追加

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="好きなonionドメインを無料で取得するためのサービスです">
+    <meta name="keywords" content="onion,tor,onionドメイン取得">
+    <link rel="icon" href="images/yuma4869_logo.png">
     <link rel="stylesheet" href="style.css">
     <title>無料独自onionドメイン取得サービス | yuma4869</title>
 </head>
